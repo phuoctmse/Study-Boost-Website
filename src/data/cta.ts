07e01ce5ebf95b4df962da219a139aa3e1581a1b
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-07-18 20:46:18 +0700

    update url

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -1,7 +1,9 @@
 export const ctaDetails = {
-    heading: 'Tham gia cùng hơn 1 nghìn người dùng để cải thiện việc học tập',
-    subheading: 'Hành trình học tập hiệu quả bắt đầu từ đây. Bắt đầu ngay hôm nay với AI trợ giảng thông minh, phương pháp Pomodoro và cộng đồng học tập sôi động!',
-    authUrl: '/auth',
-    appStoreUrl: '#',
-    googlePlayUrl: 'https://github.com/phuoctmse/Study-Boost-Website/releases/download/v1.0.0/Study_Boost.apk'
-}
+  heading: "Tham gia cùng hơn 1 nghìn người dùng để cải thiện việc học tập",
+  subheading:
+    "Hành trình học tập hiệu quả bắt đầu từ đây. Bắt đầu ngay hôm nay với AI trợ giảng thông minh, phương pháp Pomodoro và cộng đồng học tập sôi động!",
+  authUrl: "/auth",
+  appStoreUrl: "#",
+  googlePlayUrl:
+    "https://github.com/phuoctmse/Study-Boost-Website/releases/download/v2.0.0/Study_Boost_App.apk",
+};
